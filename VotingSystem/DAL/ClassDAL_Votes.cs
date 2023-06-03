057394e2691150e8369453ae53c37a7d24a46efc
Luke Symon <lukealamil12@gmail.com>
2023-06-03 16:22:34 +0800

    editing database conncetion

--- VotingSystem/DAL/ClassDAL_Votes.cs
+++ VotingSystem/DAL/ClassDAL_Votes.cs
@@ -13,136 +13,160 @@ namespace VotingSystem.DAL
     internal class ClassDAL_Votes
     {
 
-        //public bool AddItemsToTable(int voteID, int candidateID, int partylistID, int electionID, string timestamp)
-        //{
-        //    Connection con = new Connection();
-        //    if (ConnectionState.Closed == con.connect.State)
-        //    {
-        //        con.connect.Open();
-        //    }
+        private string connectionString = @"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True"; // Replace with your actual connection string
 
-        //    string query = "";
+        public bool AddItemsToTable(int voteID, string name, string partylistName, string position, Image candidatePic, Image partylistPic)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
-        //    try
-        //    {
-        //        using (SqlCommand cmd = new SqlCommand(query, con.connect))
-        //        {
-        //            cmd.Parameters.AddWithValue("@Firstname", Firstname.Trim());
-        //            cmd.Parameters.AddWithValue("@Middlename", Middlename.Trim());
-        //            cmd.Parameters.AddWithValue("@Lastname", Lastname.Trim());
-                    
-        //            cmd.Parameters.AddWithValue("@Position", Position.Trim());
-        //            cmd.Parameters.AddWithValue("@Partylist", partylist.Trim());
+                // Insert vote into Votes table
+                string insertVoteQuery = "INSERT INTO Votes (VoteID) VALUES (@VoteID);";
+                using (SqlCommand insertVoteCommand = new SqlCommand(insertVoteQuery, connection))
+                {
+                    insertVoteCommand.Parameters.AddWithValue("@VoteID", voteID);
+                    insertVoteCommand.ExecuteNonQuery();
+                }
 
+                // Get the foreign key ID from Candidates table
+                int candidateID;
+                string selectCandidateIDQuery = "SELECT CandidateID FROM Candidates WHERE Name = @Name AND Position = @Position;";
+                using (SqlCommand selectCandidateIDCommand = new SqlCommand(selectCandidateIDQuery, connection))
+                {
+                    selectCandidateIDCommand.Parameters.AddWithValue("@Name", name);
+                    selectCandidateIDCommand.Parameters.AddWithValue("@Position", position);
+                    candidateID = (int)selectCandidateIDCommand.ExecuteScalar();
+                }
 
-        //            //converting image to binary to store to database
-        //            MemoryStream ms = new MemoryStream();
-        //            CandidatePic.Save(ms, CandidatePic.RawFormat);
-        //            // set to binary format image to parameter
-        //            cmd.Parameters.AddWithValue("@CandidatePic", ms.ToArray());
+                // Get the foreign key ID from Elections table
+                int electionID;
+                string selectElectionIDQuery = "SELECT ElectionID FROM Elections WHERE Title = @Title;";
+                using (SqlCommand selectElectionIDCommand = new SqlCommand(selectElectionIDQuery, connection))
+                {
+                    selectElectionIDCommand.Parameters.AddWithValue("@Title", "YourElectionTitle"); // Replace with the actual title
+                    electionID = (int)selectElectionIDCommand.ExecuteScalar();
+                }
 
-        //            cmd.ExecuteNonQuery();
-        //        }
-        //        return true;
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-        //}
+                // Get the foreign key ID from Partylist table
+                int partylistID;
+                string selectPartylistIDQuery = "SELECT PartylistID FROM Partylist WHERE Name = @PartylistName;";
+                using (SqlCommand selectPartylistIDCommand = new SqlCommand(selectPartylistIDQuery, connection))
+                {
+                    selectPartylistIDCommand.Parameters.AddWithValue("@PartylistName", partylistName);
+                    partylistID = (int)selectPartylistIDCommand.ExecuteScalar();
+                }
 
-        ////PANG VIEW
-        //public DataTable ReadItemsTable()
-        //{
-        //    Connection con = new Connection();
-        //    if (ConnectionState.Closed == con.connect.State)
-        //    {
-        //        con.connect.Open();
-        //    }
+                // Insert into ReadItems table
+                string insertReadItemsQuery = "INSERT INTO ReadItems (VoteID, CandidateID, ElectionID, PartylistID) VALUES (@VoteID, @CandidateID, @ElectionID, @PartylistID);";
+                using (SqlCommand insertReadItemsCommand = new SqlCommand(insertReadItemsQuery, connection))
+                {
+                    insertReadItemsCommand.Parameters.AddWithValue("@VoteID", voteID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@CandidateID", candidateID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@ElectionID", electionID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@PartylistID", partylistID);
+                    insertReadItemsCommand.ExecuteNonQuery();
+                }
 
-        //    string query = "SELECT * FROM Candidates";
-        //    SqlCommand cmd = new SqlCommand(query, con.connect);
-        //    try
-        //    {
-        //        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-        //        {
-        //            DataTable dt = new DataTable();
-        //            sda.Fill(dt);
-        //            return dt;
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-        //}
+                return true;
+            }
+        }
 
-        ////PANG SEARCH
-        //public DataTable SearchItemsTable(string search)
-        //{
-        //    Connection con = new Connection();
-        //    if (ConnectionState.Closed == con.connect.State)
-        //    {
-        //        con.connect.Open();
-        //    }
+        public DataTable ReadItemsTable()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
-        //    string query = "SELECT * FROM Candidates WHERE Firstname LIKE @Search OR Lastname LIKE @Search OR Middlename LIKE @Search OR Partylist LIKE @Search OR Position LIKE @Search";
-        //    SqlCommand cmd = new SqlCommand(query, con.connect);
-        //    cmd.Parameters.AddWithValue("@Search", "%" + search + "%");
-
-        //    try
-        //    {
-        //        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-        //        {
-        //            DataTable dt = new DataTable();
-        //            sda.Fill(dt);
-        //            return dt;
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-        //}
-
-        //public bool UpdateItemInTable(int candidateID, string firstname, string middlename, string lastname, string course, string position, Image candidatePic, string partylist)
-        //{
-        //    Connection con = new Connection();
-        //    if (ConnectionState.Closed == con.connect.State)
-        //    {
-        //        con.connect.Open();
-        //    }
-
-        //    string query = "UPDATE Candidates SET Firstname = @Firstname, Middlename = @Middlename, Lastname = @Lastname, Course = @Course, Position = @Position, CandidatePic = @CandidatePic, Partylist = @Partylist  WHERE Candidate_ID = @Candidate_ID";
-
-        //    try
-        //    {
-        //        using (SqlCommand cmd = new SqlCommand(query, con.connect))
-        //        {
-        //            cmd.Parameters.AddWithValue("@Candidate_ID", candidateID);
-        //            cmd.Parameters.AddWithValue("@Firstname", firstname.Trim());
-        //            cmd.Parameters.AddWithValue("@Middlename", middlename.Trim());
-        //            cmd.Parameters.AddWithValue("@Lastname", lastname.Trim());
-        //            cmd.Parameters.AddWithValue("@Course", course.Trim());
-        //            cmd.Parameters.AddWithValue("@Position", position.Trim());
-        //            cmd.Parameters.AddWithValue("@Partylist", partylist.Trim());
-
-
-
-        //            // Converting image to binary to store in the database
-        //            MemoryStream ms = new MemoryStream();
-        //            candidatePic.Save(ms, candidatePic.RawFormat);
-        //            // Set the binary format image to the parameter
-        //            cmd.Parameters.AddWithValue("@CandidatePic", ms.ToArray());
-
-        //            cmd.ExecuteNonQuery();
-        //        }
-        //        return true;
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-        //}
+                string query = "SELECT C.Name AS CandidateName, C.Position, P.Name AS PartylistName, P.Picture AS PartylistPicture " +
+                               "FROM ReadItems R " +
+                               "JOIN Candidates C ON R.CandidateID = C.CandidateID " +
+                               "JOIN Elections E ON R.ElectionID = E.ElectionID " +
+                               "JOIN Partylist P ON R.PartylistID = P.PartylistID;";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                try
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        return dt;
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
     }
+
+    ////PANG SEARCH
+    //public DataTable SearchItemsTable(string search)
+    //    {
+    //        Connection con = new Connection();
+    //        if (ConnectionState.Closed == con.connect.State)
+    //        {
+    //            con.connect.Open();
+    //        }
+
+    //        string query = "SELECT * FROM Candidates WHERE Firstname LIKE @Search OR Lastname LIKE @Search OR Middlename LIKE @Search OR Partylist LIKE @Search OR Position LIKE @Search";
+    //        SqlCommand cmd = new SqlCommand(query, con.connect);
+    //        cmd.Parameters.AddWithValue("@Search", "%" + search + "%");
+
+    //        try
+    //        {
+    //            using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+    //            {
+    //                DataTable dt = new DataTable();
+    //                sda.Fill(dt);
+    //                return dt;
+    //            }
+    //        }
+    //        catch
+    //        {
+    //            throw;
+    //        }
+    //    }
+
+    //public bool UpdateItemInTable(int candidateID, string firstname, string middlename, string lastname, string course, string position, Image candidatePic, string partylist)
+    //{
+    //    Connection con = new Connection();
+    //    if (ConnectionState.Closed == con.connect.State)
+    //    {
+    //        con.connect.Open();
+    //    }
+
+    //    string query = "UPDATE Candidates SET Firstname = @Firstname, Middlename = @Middlename, Lastname = @Lastname, Course = @Course, Position = @Position, CandidatePic = @CandidatePic, Partylist = @Partylist  WHERE Candidate_ID = @Candidate_ID";
+
+    //    try
+    //    {
+    //        using (SqlCommand cmd = new SqlCommand(query, con.connect))
+    //        {
+    //            cmd.Parameters.AddWithValue("@Candidate_ID", candidateID);
+    //            cmd.Parameters.AddWithValue("@Firstname", firstname.Trim());
+    //            cmd.Parameters.AddWithValue("@Middlename", middlename.Trim());
+    //            cmd.Parameters.AddWithValue("@Lastname", lastname.Trim());
+    //            cmd.Parameters.AddWithValue("@Course", course.Trim());
+    //            cmd.Parameters.AddWithValue("@Position", position.Trim());
+    //            cmd.Parameters.AddWithValue("@Partylist", partylist.Trim());
+
+
+
+    //            // Converting image to binary to store in the database
+    //            MemoryStream ms = new MemoryStream();
+    //            candidatePic.Save(ms, candidatePic.RawFormat);
+    //            // Set the binary format image to the parameter
+    //            cmd.Parameters.AddWithValue("@CandidatePic", ms.ToArray());
+
+    //            cmd.ExecuteNonQuery();
+    //        }
+    //        return true;
+    //    }
+    //    catch
+    //    {
+    //        throw;
+    //    }
+    //}
+}
 }
