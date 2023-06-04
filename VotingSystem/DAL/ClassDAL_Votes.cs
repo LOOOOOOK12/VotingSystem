ad8220207ba5261f807f4c09a6db96bafc98b6ab
Luke Symon <lukealamil12@gmail.com>
2023-06-04 15:19:01 +0800

    update

--- VotingSystem/DAL/ClassDAL_Votes.cs
+++ VotingSystem/DAL/ClassDAL_Votes.cs
@@ -15,23 +15,23 @@ namespace VotingSystem.DAL
 
         private string connectionString = @"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True"; // Replace with your actual connection string
 
-        public bool AddItemsToTable(int voteID, string name, string partylistName, string position, Image candidatePic, Image partylistPic)
+        public bool AddItemsToTable(int voteID, string name, string partylistName, string position, string elecitionTitle, Image candidatePic, Image partylistPic)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
                 // Insert vote into Votes table
-                string insertVoteQuery = "INSERT INTO Votes (VoteID) VALUES (@VoteID);";
+                string insertVoteQuery = "INSERT INTO Votes (Vote_ID) VALUES (@Vote_ID);";
                 using (SqlCommand insertVoteCommand = new SqlCommand(insertVoteQuery, connection))
                 {
-                    insertVoteCommand.Parameters.AddWithValue("@VoteID", voteID);
+                    insertVoteCommand.Parameters.AddWithValue("@Vote_ID", voteID);
                     insertVoteCommand.ExecuteNonQuery();
                 }
 
                 // Get the foreign key ID from Candidates table
                 int candidateID;
-                string selectCandidateIDQuery = "SELECT CandidateID FROM Candidates WHERE Name = @Name AND Position = @Position;";
+                string selectCandidateIDQuery = "SELECT Candidate_ID FROM Candidates WHERE Name = @Name AND Position = @Position;";
                 using (SqlCommand selectCandidateIDCommand = new SqlCommand(selectCandidateIDQuery, connection))
                 {
                     selectCandidateIDCommand.Parameters.AddWithValue("@Name", name);
@@ -41,16 +41,16 @@ namespace VotingSystem.DAL
 
                 // Get the foreign key ID from Elections table
                 int electionID;
-                string selectElectionIDQuery = "SELECT ElectionID FROM Elections WHERE Title = @Title;";
+                string selectElectionIDQuery = "SELECT Election_ID FROM Election WHERE ElectionTitle = @ElectionTitle;";
                 using (SqlCommand selectElectionIDCommand = new SqlCommand(selectElectionIDQuery, connection))
                 {
-                    selectElectionIDCommand.Parameters.AddWithValue("@Title", "YourElectionTitle"); // Replace with the actual title
+                    selectElectionIDCommand.Parameters.AddWithValue("@ElectionTitle", "ElectionTitle"); // Replace with the actual title
                     electionID = (int)selectElectionIDCommand.ExecuteScalar();
                 }
 
                 // Get the foreign key ID from Partylist table
                 int partylistID;
-                string selectPartylistIDQuery = "SELECT PartylistID FROM Partylist WHERE Name = @PartylistName;";
+                string selectPartylistIDQuery = "SELECT Partylist_ID FROM Partylist WHERE PartylistName = @PartylistName;";
                 using (SqlCommand selectPartylistIDCommand = new SqlCommand(selectPartylistIDQuery, connection))
                 {
                     selectPartylistIDCommand.Parameters.AddWithValue("@PartylistName", partylistName);
@@ -58,13 +58,13 @@ namespace VotingSystem.DAL
                 }
 
                 // Insert into ReadItems table
-                string insertReadItemsQuery = "INSERT INTO ReadItems (VoteID, CandidateID, ElectionID, PartylistID) VALUES (@VoteID, @CandidateID, @ElectionID, @PartylistID);";
+                string insertReadItemsQuery = "INSERT INTO ReadItems (Vote_ID, Candidate_ID, Election_ID, Partylist_ID) VALUES (@Vote_ID, @Candidate_ID, @Election_ID, @Partylist_ID);";
                 using (SqlCommand insertReadItemsCommand = new SqlCommand(insertReadItemsQuery, connection))
                 {
-                    insertReadItemsCommand.Parameters.AddWithValue("@VoteID", voteID);
-                    insertReadItemsCommand.Parameters.AddWithValue("@CandidateID", candidateID);
-                    insertReadItemsCommand.Parameters.AddWithValue("@ElectionID", electionID);
-                    insertReadItemsCommand.Parameters.AddWithValue("@PartylistID", partylistID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@Vote_ID", voteID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@Candidate_ID", candidateID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@Election_ID", electionID);
+                    insertReadItemsCommand.Parameters.AddWithValue("@Partylist_ID", partylistID);
                     insertReadItemsCommand.ExecuteNonQuery();
                 }
 
@@ -78,11 +78,11 @@ namespace VotingSystem.DAL
             {
                 connection.Open();
 
-                string query = "SELECT C.Name AS CandidateName, C.Position, P.Name AS PartylistName, P.Picture AS PartylistPicture " +
-                               "FROM ReadItems R " +
-                               "JOIN Candidates C ON R.CandidateID = C.CandidateID " +
-                               "JOIN Elections E ON R.ElectionID = E.ElectionID " +
-                               "JOIN Partylist P ON R.PartylistID = P.PartylistID;";
+                string query = "SELECT C.Name AS Name, C.Position, P.PartylistName AS PartylistName, P.PartylistLogo AS PartylistLogo, C.CandidatePic AS CandidatePic " +
+                           "FROM Votes V " +
+                           "INNER JOIN Candidates C ON V.Candidate_ID = C.Candidate_ID " +
+                           "INNER JOIN Election E ON V.Election_ID = E.Election_ID " +
+                           "INNER JOIN Partylist P ON V.Partylist_ID = P.Partylist_ID;";
                 SqlCommand cmd = new SqlCommand(query, connection);
                 try
                 {
@@ -169,4 +169,4 @@ namespace VotingSystem.DAL
     //    }
     //}
 }
-}
+
