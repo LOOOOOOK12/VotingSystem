b1385de0fd275e7793c2f821fd3b9c63a175ab46
Luke Symon <lukealamil12@gmail.com>
2023-06-04 15:18:40 +0800

    update

--- VotingSystem/BLL/ClassBLL_Votes.cs
+++ VotingSystem/BLL/ClassBLL_Votes.cs
@@ -13,16 +13,16 @@ namespace VotingSystem.BLL
     internal class ClassBLL_Votes
     {
 
-        public bool SaveItems(int voteID, string name, string partylistName, string position, Image candidatePic, Image partylistPic)
+        public bool SaveItems(int voteID, string name, string partylistName, string position, string electionTitle, Image candidatePic, Image partylistPic)
         {
             try
             {
                 ClassDAL_Votes objdalVotes = new ClassDAL_Votes();
-                return objdalVotes.AddItemsToTable(voteID,name,partylistName,position,candidatePic,partylistPic);
+                return objdalVotes.AddItemsToTable(voteID, name, partylistName, position, electionTitle, candidatePic, partylistPic);
             }
             catch (Exception e)
             {
-                DialogResult result = MessageBox.Show(e.Message.ToString());
+                MessageBox.Show(e.Message.ToString());
                 return false;
             }
         }
@@ -36,37 +36,38 @@ namespace VotingSystem.BLL
             }
             catch (Exception e)
             {
-                DialogResult result = MessageBox.Show(e.Message.ToString());
+                MessageBox.Show(e.Message.ToString());
                 return null;
             }
         }
-
-        //public DataTable SearchItems(string search)
-        //{
-        //    try
-        //    {
-        //        ClassDAL_Candidates objDALC = new ClassDAL_Candidates();
-        //        return objDALC.SearchItemsTable(search);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        DialogResult result = MessageBox.Show(e.Message.ToString());
-        //        return null;
-        //    }
-        //}
-
-        //public bool UpdateItems(int candidateID, string Firstname, string Middlename, string Lastname, string Course, string Position, Image CandidatePic, string partylist)
-        //{
-        //    try
-        //    {
-        //        ClassDAL_Candidates objdal = new ClassDAL_Candidates();
-        //        return objdal.UpdateItemInTable(candidateID, Firstname, Middlename, Lastname, Course, Position, CandidatePic, partylist);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        MessageBox.Show(e.Message.ToString());
-        //        return false;
-        //    }
-        //}
     }
+
+    //public DataTable SearchItems(string search)
+    //{
+    //    try
+    //    {
+    //        ClassDAL_Candidates objDALC = new ClassDAL_Candidates();
+    //        return objDALC.SearchItemsTable(search);
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        DialogResult result = MessageBox.Show(e.Message.ToString());
+    //        return null;
+    //    }
+    //}
+
+    //public bool UpdateItems(int candidateID, string Firstname, string Middlename, string Lastname, string Course, string Position, Image CandidatePic, string partylist)
+    //{
+    //    try
+    //    {
+    //        ClassDAL_Candidates objdal = new ClassDAL_Candidates();
+    //        return objdal.UpdateItemInTable(candidateID, Firstname, Middlename, Lastname, Course, Position, CandidatePic, partylist);
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        MessageBox.Show(e.Message.ToString());
+    //        return false;
+    //    }
+    //}
 }
+
