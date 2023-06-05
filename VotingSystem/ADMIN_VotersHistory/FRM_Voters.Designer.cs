65c5fd1bc360144f8d9b01758bf8a59034ae084b
Luke Symon <lukealamil12@gmail.com>
2023-06-05 21:38:45 +0800

    VotersHistory

---
+++ VotingSystem/ADMIN_VotersHistory/FRM_Voters.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace VotingSystem
+{
+    partial class FRM_Voters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TXTBX_SearchCandidates = new System.Windows.Forms.TextBox();
+            this.BTN_Delete = new System.Windows.Forms.Button();
+            this.BTN_Update = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Refresh_btn = new System.Windows.Forms.Button();
+            this.Back_btn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.TXTBX_SearchCandidates);
+            this.panel1.Controls.Add(this.BTN_Delete);
+            this.panel1.Controls.Add(this.BTN_Update);
+            this.panel1.Controls.Add(this.Refresh_btn);
+            this.panel1.Controls.Add(this.Back_btn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(835, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
+            this.button1.Location = new System.Drawing.Point(253, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 36);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Search\r\n";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // TXTBX_SearchCandidates
+            // 
+            this.TXTBX_SearchCandidates.Location = new System.Drawing.Point(377, 70);
+            this.TXTBX_SearchCandidates.Name = "TXTBX_SearchCandidates";
+            this.TXTBX_SearchCandidates.Size = new System.Drawing.Size(165, 20);
+            this.TXTBX_SearchCandidates.TabIndex = 15;
+            this.TXTBX_SearchCandidates.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TXTBX_SearchCandidates.TextChanged += new System.EventHandler(this.TXTBX_SearchCandidates_TextChanged);
+            // 
+            // BTN_Delete
+            // 
+            this.BTN_Delete.BackColor = System.Drawing.Color.Yellow;
+            this.BTN_Delete.FlatAppearance.BorderSize = 0;
+            this.BTN_Delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Delete.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Delete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
+            this.BTN_Delete.Location = new System.Drawing.Point(129, 60);
+            this.BTN_Delete.Name = "BTN_Delete";
+            this.BTN_Delete.Size = new System.Drawing.Size(118, 36);
+            this.BTN_Delete.TabIndex = 14;
+            this.BTN_Delete.Text = "Delete";
+            this.BTN_Delete.UseVisualStyleBackColor = false;
+            this.BTN_Delete.Click += new System.EventHandler(this.BTN_SearchCandidates_Click);
+            // 
+            // BTN_Update
+            // 
+            this.BTN_Update.BackColor = System.Drawing.Color.Yellow;
+            this.BTN_Update.FlatAppearance.BorderSize = 0;
+            this.BTN_Update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Update.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Update.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
+            this.BTN_Update.Location = new System.Drawing.Point(12, 60);
+            this.BTN_Update.Name = "BTN_Update";
+            this.BTN_Update.Size = new System.Drawing.Size(111, 36);
+            this.BTN_Update.TabIndex = 13;
+            this.BTN_Update.Text = "Update";
+            this.BTN_Update.UseVisualStyleBackColor = false;
+            this.BTN_Update.Click += new System.EventHandler(this.BTN_Update_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(341, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Voters History\r\n";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(811, 381);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Refresh_btn
+            // 
+            this.Refresh_btn.FlatAppearance.BorderSize = 0;
+            this.Refresh_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Refresh_btn.Image = global::VotingSystem.Properties.Resources.icons8_refresh_24;
+            this.Refresh_btn.Location = new System.Drawing.Point(726, 54);
+            this.Refresh_btn.Name = "Refresh_btn";
+            this.Refresh_btn.Size = new System.Drawing.Size(45, 42);
+            this.Refresh_btn.TabIndex = 10;
+            this.Refresh_btn.Text = " ";
+            this.Refresh_btn.UseVisualStyleBackColor = true;
+            // 
+            // Back_btn
+            // 
+            this.Back_btn.FlatAppearance.BorderSize = 0;
+            this.Back_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back_btn.Image = global::VotingSystem.Properties.Resources.icons8_left_50;
+            this.Back_btn.Location = new System.Drawing.Point(777, 54);
+            this.Back_btn.Name = "Back_btn";
+            this.Back_btn.Size = new System.Drawing.Size(46, 36);
+            this.Back_btn.TabIndex = 9;
+            this.Back_btn.Text = " ";
+            this.Back_btn.UseVisualStyleBackColor = true;
+            this.Back_btn.Click += new System.EventHandler(this.Back_btn_Click);
+            // 
+            // FRM_Voters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRM_Voters";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRM_Voters";
+            this.Load += new System.EventHandler(this.FRM_Voters_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Back_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Refresh_btn;
+        private System.Windows.Forms.TextBox TXTBX_SearchCandidates;
+        private System.Windows.Forms.Button BTN_Delete;
+        private System.Windows.Forms.Button BTN_Update;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ VotingSystem/ADMIN_VotersHistory/FRM_Voters.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VotingSystem
+{
+    public partial class FRM_Voters : Form
+    {
+
+
+        public FRM_Voters()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_btn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void FRM_Voters_Load(object sender, EventArgs e)
+        {
+            string con = @"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(con))
+            { 
+                connection.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter("SELECT * FROM Voters ", connection);
+                DataTable table =  new DataTable();
+                sqlData.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BTN_SearchCandidates_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TXTBX_SearchCandidates_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTN_Update_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True");
+            
+        }
+    }
+}
