5b97f38cc394ded32b6ba0d379ec48cb1b868a1c
Luke Symon <lukealamil12@gmail.com>
2023-06-06 20:57:56 +0800

    Work in progress

--- VotingSystem/DAL/ClassDAL_Votes.cs
+++ VotingSystem/DAL/ClassDAL_Votes.cs
@@ -12,22 +12,13 @@ namespace VotingSystem.DAL
 {
     internal class ClassDAL_Votes
     {
-
         private string connectionString = @"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True"; // Replace with your actual connection string
 
-        public bool AddItemsToTable(int voteID, string name, string partylistName, string position, string elecitionTitle, Image candidatePic, Image partylistPic)
+        public bool AddItemsToTable(int voteID,  string name, string partylistName, string position, string electionTitle, Image candidatePic, Image partylistPic)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-
-                // Insert vote into Votes table
-                string insertVoteQuery = "INSERT INTO Votes (Vote_ID) VALUES (@Vote_ID);";
-                using (SqlCommand insertVoteCommand = new SqlCommand(insertVoteQuery, connection))
-                {
-                    insertVoteCommand.Parameters.AddWithValue("@Vote_ID", voteID);
-                    insertVoteCommand.ExecuteNonQuery();
-                }
 
                 // Get the foreign key ID from Candidates table
                 int candidateID;
@@ -44,7 +35,7 @@ namespace VotingSystem.DAL
                 string selectElectionIDQuery = "SELECT Election_ID FROM Election WHERE ElectionTitle = @ElectionTitle;";
                 using (SqlCommand selectElectionIDCommand = new SqlCommand(selectElectionIDQuery, connection))
                 {
-                    selectElectionIDCommand.Parameters.AddWithValue("@ElectionTitle", "ElectionTitle"); // Replace with the actual title
+                    selectElectionIDCommand.Parameters.AddWithValue("@ElectionTitle", electionTitle);
                     electionID = (int)selectElectionIDCommand.ExecuteScalar();
                 }
 
@@ -57,48 +48,49 @@ namespace VotingSystem.DAL
                     partylistID = (int)selectPartylistIDCommand.ExecuteScalar();
                 }
 
-                // Insert into ReadItems table
-                string insertReadItemsQuery = "INSERT INTO ReadItems (Vote_ID, Candidate_ID, Election_ID, Partylist_ID) VALUES (@Vote_ID, @Candidate_ID, @Election_ID, @Partylist_ID);";
-                using (SqlCommand insertReadItemsCommand = new SqlCommand(insertReadItemsQuery, connection))
+                // Insert into Votes table
+                string insertVoteQuery = "INSERT INTO Votes (Vote_ID, Candidate_ID, Election_ID, Partylist_ID) VALUES (@Vote_ID, @Candidate_ID, @Election_ID, @Partylist_ID);";
+                using (SqlCommand insertVoteCommand = new SqlCommand(insertVoteQuery, connection))
                 {
-                    insertReadItemsCommand.Parameters.AddWithValue("@Vote_ID", voteID);
-                    insertReadItemsCommand.Parameters.AddWithValue("@Candidate_ID", candidateID);
-                    insertReadItemsCommand.Parameters.AddWithValue("@Election_ID", electionID);
-                    insertReadItemsCommand.Parameters.AddWithValue("@Partylist_ID", partylistID);
-                    insertReadItemsCommand.ExecuteNonQuery();
+                    insertVoteCommand.Parameters.AddWithValue("@Vote_ID", voteID);
+                    insertVoteCommand.Parameters.AddWithValue("@Candidate_ID", candidateID);
+                    insertVoteCommand.Parameters.AddWithValue("@Election_ID", electionID);
+                    insertVoteCommand.Parameters.AddWithValue("@Partylist_ID", partylistID);
+                    insertVoteCommand.ExecuteNonQuery();
                 }
+
 
                 return true;
             }
         }
 
-        public DataTable ReadItemsTable()
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
+       public DataTable ReadItemsTable()
+{
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
 
-                string query = "SELECT C.Name AS Name, C.Position, P.PartylistName AS PartylistName, P.PartylistLogo AS PartylistLogo, C.CandidatePic AS CandidatePic " +
-                           "FROM Votes V " +
-                           "INNER JOIN Candidates C ON V.Candidate_ID = C.Candidate_ID " +
-                           "INNER JOIN Election E ON V.Election_ID = E.Election_ID " +
-                           "INNER JOIN Partylist P ON V.Partylist_ID = P.Partylist_ID;";
-                SqlCommand cmd = new SqlCommand(query, connection);
-                try
-                {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable dt = new DataTable();
-                        sda.Fill(dt);
-                        return dt;
-                    }
-                }
-                catch
-                {
-                    throw;
-                }
+        string query = "SELECT C.Name AS Name, C.Position AS Position, P.PartylistName AS PartylistName, P.PartylistLogo AS PartylistLogo, C.CandidatePic AS CandidatePic " +
+                       "FROM Votes V " +
+                       "INNER JOIN Candidates C ON V.Candidate_ID = C.Candidate_ID " +
+                       "INNER JOIN Election E ON V.Election_ID = E.Election_ID " +
+                       "INNER JOIN Partylist P ON V.Partylist_ID = P.Partylist_ID;";
+        SqlCommand cmd = new SqlCommand(query, connection);
+        try
+        {
+            using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+            {
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                return dt;
             }
         }
+        catch
+        {
+            throw;
+        }
+    }
+}
     }
 
     ////PANG SEARCH
