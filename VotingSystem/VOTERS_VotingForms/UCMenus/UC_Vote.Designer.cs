3f6044982311d512291d72a575216b36768349d8
Luke Symon <lukealamil12@gmail.com>
2023-06-06 21:00:13 +0800

    update

--- VotingSystem/VOTERS_VotingForms/UCMenus/UC_Vote.Designer.cs
+++ VotingSystem/VOTERS_VotingForms/UCMenus/UC_Vote.Designer.cs
@@ -29,44 +29,30 @@
         private void InitializeComponent()
         {
             this.FL_Vote = new System.Windows.Forms.FlowLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FL_Vote
             // 
-            this.FL_Vote.Location = new System.Drawing.Point(0, 63);
+            this.FL_Vote.AutoScroll = true;
+            this.FL_Vote.Location = new System.Drawing.Point(0, 0);
             this.FL_Vote.Name = "FL_Vote";
-            this.FL_Vote.Size = new System.Drawing.Size(650, 322);
+            this.FL_Vote.Size = new System.Drawing.Size(650, 385);
             this.FL_Vote.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
-            this.label1.Location = new System.Drawing.Point(283, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 36);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Vote";
             // 
             // UC_Vote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.FL_Vote);
             this.Name = "UC_Vote";
             this.Size = new System.Drawing.Size(650, 388);
             this.Load += new System.EventHandler(this.UC_Vote_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel FL_Vote;
-        private System.Windows.Forms.Label label1;
     }
 }
