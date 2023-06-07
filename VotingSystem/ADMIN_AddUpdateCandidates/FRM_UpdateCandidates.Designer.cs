4b87b20df40c19768c56ffcdecd3ecb154e5cc2f
Luke Symon <lukealamil12@gmail.com>
2023-06-07 13:58:23 +0800

    fixed update function

--- VotingSystem/ADMIN_AddUpdateCandidates/FRM_UpdateCandidates.Designer.cs
+++ VotingSystem/ADMIN_AddUpdateCandidates/FRM_UpdateCandidates.Designer.cs
@@ -34,11 +34,9 @@
             this.CB_Position = new System.Windows.Forms.ComboBox();
             this.CB_Course = new System.Windows.Forms.ComboBox();
             this.TXTBX_Name = new System.Windows.Forms.TextBox();
-            this.CB_Year = new System.Windows.Forms.ComboBox();
             this.CB_Partylist = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.BTN_InsertPhoto = new System.Windows.Forms.Button();
@@ -60,14 +58,14 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(690, 70);
+            this.panel1.Size = new System.Drawing.Size(526, 70);
             this.panel1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(231, 18);
+            this.label1.Location = new System.Drawing.Point(150, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(245, 38);
             this.label1.TabIndex = 0;
@@ -84,57 +82,44 @@
             "2nd year representative",
             "3rd year representative",
             "4th year representative"});
-            this.CB_Position.Location = new System.Drawing.Point(359, 232);
+            this.CB_Position.Location = new System.Drawing.Point(327, 169);
             this.CB_Position.Name = "CB_Position";
-            this.CB_Position.Size = new System.Drawing.Size(152, 21);
+            this.CB_Position.Size = new System.Drawing.Size(187, 21);
             this.CB_Position.TabIndex = 35;
             // 
             // CB_Course
             // 
             this.CB_Course.FormattingEnabled = true;
             this.CB_Course.Items.AddRange(new object[] {
-            "Bachelor of Science in Information Technology",
-            "Bachelore of Science in Computer Engineering",
-            "Bachelore of Science in Hospitality Management",
-            "Bachelore of Science in Business Administration",
-            "Bachelor of Science in Accounting Information Systems"});
-            this.CB_Course.Location = new System.Drawing.Point(179, 169);
+            "BSIT",
+            "BSCpE",
+            "BSHM",
+            "BSBA",
+            "BSAIS"});
+            this.CB_Course.Location = new System.Drawing.Point(179, 232);
             this.CB_Course.Name = "CB_Course";
-            this.CB_Course.Size = new System.Drawing.Size(154, 21);
+            this.CB_Course.Size = new System.Drawing.Size(142, 21);
             this.CB_Course.TabIndex = 34;
             // 
             // TXTBX_Name
             // 
-            this.TXTBX_Name.Location = new System.Drawing.Point(359, 106);
+            this.TXTBX_Name.Location = new System.Drawing.Point(179, 169);
             this.TXTBX_Name.Name = "TXTBX_Name";
-            this.TXTBX_Name.Size = new System.Drawing.Size(152, 20);
+            this.TXTBX_Name.Size = new System.Drawing.Size(142, 20);
             this.TXTBX_Name.TabIndex = 32;
-            // 
-            // CB_Year
-            // 
-            this.CB_Year.FormattingEnabled = true;
-            this.CB_Year.Items.AddRange(new object[] {
-            "1st",
-            "2nd",
-            "3rd",
-            "4th"});
-            this.CB_Year.Location = new System.Drawing.Point(359, 169);
-            this.CB_Year.Name = "CB_Year";
-            this.CB_Year.Size = new System.Drawing.Size(152, 21);
-            this.CB_Year.TabIndex = 30;
             // 
             // CB_Partylist
             // 
             this.CB_Partylist.FormattingEnabled = true;
-            this.CB_Partylist.Location = new System.Drawing.Point(179, 232);
+            this.CB_Partylist.Location = new System.Drawing.Point(327, 232);
             this.CB_Partylist.Name = "CB_Partylist";
-            this.CB_Partylist.Size = new System.Drawing.Size(154, 21);
+            this.CB_Partylist.Size = new System.Drawing.Size(187, 21);
             this.CB_Partylist.TabIndex = 29;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(176, 216);
+            this.label9.Location = new System.Drawing.Point(327, 216);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(50, 13);
             this.label9.TabIndex = 28;
@@ -143,25 +128,16 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(356, 216);
+            this.label8.Location = new System.Drawing.Point(327, 153);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(47, 13);
             this.label8.TabIndex = 27;
             this.label8.Text = "Position:";
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(356, 153);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(32, 13);
-            this.label7.TabIndex = 26;
-            this.label7.Text = "Year:";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(179, 153);
+            this.label6.Location = new System.Drawing.Point(179, 216);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(43, 13);
             this.label6.TabIndex = 25;
@@ -170,7 +146,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(356, 90);
+            this.label3.Location = new System.Drawing.Point(176, 153);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 22;
@@ -194,7 +170,7 @@
             this.BTN_Cancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
             this.BTN_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BTN_Cancel.ForeColor = System.Drawing.Color.White;
-            this.BTN_Cancel.Location = new System.Drawing.Point(583, 314);
+            this.BTN_Cancel.Location = new System.Drawing.Point(419, 314);
             this.BTN_Cancel.Name = "BTN_Cancel";
             this.BTN_Cancel.Size = new System.Drawing.Size(95, 32);
             this.BTN_Cancel.TabIndex = 37;
@@ -207,7 +183,7 @@
             this.BTN_UpdateCandidate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
             this.BTN_UpdateCandidate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BTN_UpdateCandidate.ForeColor = System.Drawing.Color.White;
-            this.BTN_UpdateCandidate.Location = new System.Drawing.Point(468, 314);
+            this.BTN_UpdateCandidate.Location = new System.Drawing.Point(304, 314);
             this.BTN_UpdateCandidate.Name = "BTN_UpdateCandidate";
             this.BTN_UpdateCandidate.Size = new System.Drawing.Size(109, 32);
             this.BTN_UpdateCandidate.TabIndex = 36;
@@ -219,7 +195,7 @@
             // 
             this.TXTBX_CandidateID.Location = new System.Drawing.Point(179, 106);
             this.TXTBX_CandidateID.Name = "TXTBX_CandidateID";
-            this.TXTBX_CandidateID.Size = new System.Drawing.Size(154, 20);
+            this.TXTBX_CandidateID.Size = new System.Drawing.Size(142, 20);
             this.TXTBX_CandidateID.TabIndex = 39;
             // 
             // label2
@@ -244,33 +220,25 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(523, 90);
+            this.label4.Location = new System.Drawing.Point(324, 90);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 40;
-            this.label4.Text = "Election";
+            this.label4.Text = "Election Title:";
             // 
             // CB_Elections
             // 
             this.CB_Elections.FormattingEnabled = true;
-            this.CB_Elections.Items.AddRange(new object[] {
-            "President",
-            "Vice-President",
-            "Secretary",
-            "1st year representative",
-            "2nd year representative",
-            "3rd year representative",
-            "4th year representative"});
-            this.CB_Elections.Location = new System.Drawing.Point(526, 106);
+            this.CB_Elections.Location = new System.Drawing.Point(327, 106);
             this.CB_Elections.Name = "CB_Elections";
-            this.CB_Elections.Size = new System.Drawing.Size(152, 21);
+            this.CB_Elections.Size = new System.Drawing.Size(187, 21);
             this.CB_Elections.TabIndex = 42;
             // 
             // FRM_UpdateCandidates
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 358);
+            this.ClientSize = new System.Drawing.Size(526, 358);
             this.Controls.Add(this.CB_Elections);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.TXTBX_CandidateID);
@@ -284,11 +252,9 @@
             this.Controls.Add(this.BTN_InsertPhoto);
             this.Controls.Add(this.TXTBX_Name);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.CB_Year);
             this.Controls.Add(this.CB_Partylist);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FRM_UpdateCandidates";
@@ -309,11 +275,9 @@
         private System.Windows.Forms.ComboBox CB_Position;
         private System.Windows.Forms.ComboBox CB_Course;
         private System.Windows.Forms.TextBox TXTBX_Name;
-        private System.Windows.Forms.ComboBox CB_Year;
         private System.Windows.Forms.ComboBox CB_Partylist;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button BTN_InsertPhoto;
