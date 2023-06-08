dd6419e116b6f142e67cb6cd7c232cd12959d984
Luke Symon <lukealamil12@gmail.com>
2023-06-08 21:52:56 +0800

    update

--- VotingSystem/Properties/Resources.Designer.cs
+++ VotingSystem/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace VotingSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_eye_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-eye-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_left_50 {
             get {
                 object obj = ResourceManager.GetObject("icons8-left-50", resourceCulture);
