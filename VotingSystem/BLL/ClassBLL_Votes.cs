e5f80c00599d0b070fe904165af0b982cb2de171
Luke Symon <lukealamil12@gmail.com>
2023-06-10 15:50:32 +0800

    add items to db

--- VotingSystem/BLL/ClassBLL_Votes.cs
+++ VotingSystem/BLL/ClassBLL_Votes.cs
@@ -13,62 +13,63 @@ namespace VotingSystem.BLL
     internal class ClassBLL_Votes
     {
 
-        //    public bool SaveItems(int voteID, string name, string partylistName, string position, string electionTitle, Image candidatePic, Image partylistPic)
-        //    {
-        //        try
-        //        {
-        //            ClassDAL_Votes objdalVotes = new ClassDAL_Votes();
-        //            return objdalVotes.AddItemsToTable(voteID, name, partylistName, position, electionTitle, candidatePic, partylistPic);
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            MessageBox.Show(e.Message.ToString());
-        //            return false;
-        //        }
-        //    }
+        public bool SaveItems(string name, string partylistName, string position, string electionTitle, string timestamp)
+        {
+            try
+            {
+                ClassDAL_Votes objdalVotes = new ClassDAL_Votes();
+                return objdalVotes.AddItemsToTable(name, partylistName, position, electionTitle, timestamp);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+                return false;
+            }
+        }
 
-        //    public DataTable GetItems()
-        //    {
-        //        try
-        //        {
-        //            ClassDAL_Votes objdalVotes = new ClassDAL_Votes();
-        //            return objdalVotes.ReadItemsTable();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            MessageBox.Show(e.Message.ToString());
-        //            return null;
-        //        }
-        //    }
-        //}
-
-        //public DataTable SearchItems(string search)
-        //{
-        //    try
-        //    {
-        //        ClassDAL_Candidates objDALC = new ClassDAL_Candidates();
-        //        return objDALC.SearchItemsTable(search);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        DialogResult result = MessageBox.Show(e.Message.ToString());
-        //        return null;
-        //    }
-        //}
-
-        //public bool UpdateItems(int candidateID, string Firstname, string Middlename, string Lastname, string Course, string Position, Image CandidatePic, string partylist)
-        //{
-        //    try
-        //    {
-        //        ClassDAL_Candidates objdal = new ClassDAL_Candidates();
-        //        return objdal.UpdateItemInTable(candidateID, Firstname, Middlename, Lastname, Course, Position, CandidatePic, partylist);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        MessageBox.Show(e.Message.ToString());
-        //        return false;
-        //    }
-        //}
+        public DataTable GetItems()
+        {
+            try
+            {
+                ClassDAL_Votes objdalVotes = new ClassDAL_Votes();
+                return objdalVotes.ReadItemsTable();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+                return null;
+            }
+        }
     }
+
+    //public DataTable SearchItems(string search)
+    //{
+    //    try
+    //    {
+    //        ClassDAL_Candidates objDALC = new ClassDAL_Candidates();
+    //        return objDALC.SearchItemsTable(search);
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        DialogResult result = MessageBox.Show(e.Message.ToString());
+    //        return null;
+    //    }
+    //}
+
+    //public bool UpdateItems(int candidateID, string Firstname, string Middlename, string Lastname, string Course, string Position, Image CandidatePic, string partylist)
+    //{
+    //    try
+    //    {
+    //        ClassDAL_Candidates objdal = new ClassDAL_Candidates();
+    //        return objdal.UpdateItemInTable(candidateID, Firstname, Middlename, Lastname, Course, Position, CandidatePic, partylist);
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        MessageBox.Show(e.Message.ToString());
+    //        return false;
+    //    }
+    //}
 }
+
+
 
