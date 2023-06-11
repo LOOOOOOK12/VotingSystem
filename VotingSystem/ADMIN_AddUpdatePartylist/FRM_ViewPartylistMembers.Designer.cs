dc4d6c0f93af88ef2690b97ea5eb1e9f14b2ff29
Luke Symon <lukealamil12@gmail.com>
2023-06-11 16:34:20 +0800

    design update

--- VotingSystem/ADMIN_AddUpdatePartylist/FRM_ViewPartylistMembers.Designer.cs
+++ VotingSystem/ADMIN_AddUpdatePartylist/FRM_ViewPartylistMembers.Designer.cs
@@ -29,40 +29,33 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.PB_Partylist = new System.Windows.Forms.PictureBox();
             this.Back_btn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.FL_Members = new System.Windows.Forms.FlowLayoutPanel();
+            this.Refresh_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.PB_Partylist)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
-            this.panel1.Controls.Add(this.PB_Partylist);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Refresh_btn);
             this.panel1.Controls.Add(this.Back_btn);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(399, 70);
+            this.panel1.Size = new System.Drawing.Size(399, 90);
             this.panel1.TabIndex = 1;
-            // 
-            // PB_Partylist
-            // 
-            this.PB_Partylist.Location = new System.Drawing.Point(3, 0);
-            this.PB_Partylist.Name = "PB_Partylist";
-            this.PB_Partylist.Size = new System.Drawing.Size(79, 70);
-            this.PB_Partylist.TabIndex = 10;
-            this.PB_Partylist.TabStop = false;
             // 
             // Back_btn
             // 
             this.Back_btn.FlatAppearance.BorderSize = 0;
             this.Back_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Back_btn.Image = global::VotingSystem.Properties.Resources.icons8_left_50;
-            this.Back_btn.Location = new System.Drawing.Point(353, 34);
+            this.Back_btn.Location = new System.Drawing.Point(353, 44);
             this.Back_btn.Name = "Back_btn";
             this.Back_btn.Size = new System.Drawing.Size(46, 36);
             this.Back_btn.TabIndex = 9;
@@ -74,7 +67,7 @@
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(137, 18);
+            this.label1.Location = new System.Drawing.Point(137, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(132, 38);
             this.label1.TabIndex = 0;
@@ -84,24 +77,47 @@
             // 
             this.FL_Members.AutoScroll = true;
             this.FL_Members.BackColor = System.Drawing.SystemColors.Menu;
-            this.FL_Members.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.FL_Members.Location = new System.Drawing.Point(0, 70);
+            this.FL_Members.Location = new System.Drawing.Point(0, 86);
             this.FL_Members.Name = "FL_Members";
-            this.FL_Members.Size = new System.Drawing.Size(399, 249);
+            this.FL_Members.Size = new System.Drawing.Size(399, 274);
             this.FL_Members.TabIndex = 2;
+            // 
+            // Refresh_btn
+            // 
+            this.Refresh_btn.FlatAppearance.BorderSize = 0;
+            this.Refresh_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Refresh_btn.Image = global::VotingSystem.Properties.Resources.icons8_refresh_24;
+            this.Refresh_btn.Location = new System.Drawing.Point(310, 46);
+            this.Refresh_btn.Name = "Refresh_btn";
+            this.Refresh_btn.Size = new System.Drawing.Size(37, 36);
+            this.Refresh_btn.TabIndex = 11;
+            this.Refresh_btn.Text = " ";
+            this.Refresh_btn.UseVisualStyleBackColor = true;
+            this.Refresh_btn.Click += new System.EventHandler(this.Refresh_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(157, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 38);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "PL ID";
             // 
             // FRM_ViewPartylistMembers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 319);
+            this.ClientSize = new System.Drawing.Size(399, 360);
             this.Controls.Add(this.FL_Members);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FRM_ViewPartylistMembers";
             this.Load += new System.EventHandler(this.FRM_ViewPartylistMembers_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.PB_Partylist)).EndInit();
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -112,6 +128,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Back_btn;
         private System.Windows.Forms.FlowLayoutPanel FL_Members;
-        private System.Windows.Forms.PictureBox PB_Partylist;
+        private System.Windows.Forms.Button Refresh_btn;
+        private System.Windows.Forms.Label label2;
     }
 }
