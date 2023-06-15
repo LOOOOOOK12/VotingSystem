de9c526d45418da8bf974b3fe45ad23905a17b26
Luke Symon <lukealamil12@gmail.com>
2023-06-15 18:25:30 +0800

    added View Election Partylist

---
+++ VotingSystem/ADMIN_AddUpdateElection/FRM_ViewElectionParticipants.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace VotingSystem.ADMIN_AddUpdateElection
+{
+    partial class FRM_ViewElectionParticipants
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FL_Elections = new System.Windows.Forms.FlowLayoutPanel();
+            this.Refresh_btn = new System.Windows.Forms.Button();
+            this.Back_btn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(188)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Refresh_btn);
+            this.panel1.Controls.Add(this.Back_btn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 90);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(123, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 38);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Election ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(137, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Members";
+            // 
+            // FL_Elections
+            // 
+            this.FL_Elections.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FL_Elections.Location = new System.Drawing.Point(0, 90);
+            this.FL_Elections.Name = "FL_Elections";
+            this.FL_Elections.Size = new System.Drawing.Size(399, 270);
+            this.FL_Elections.TabIndex = 3;
+            this.FL_Elections.Paint += new System.Windows.Forms.PaintEventHandler(this.FL_Elections_Paint);
+            // 
+            // Refresh_btn
+            // 
+            this.Refresh_btn.FlatAppearance.BorderSize = 0;
+            this.Refresh_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Refresh_btn.Image = global::VotingSystem.Properties.Resources.icons8_refresh_24;
+            this.Refresh_btn.Location = new System.Drawing.Point(310, 46);
+            this.Refresh_btn.Name = "Refresh_btn";
+            this.Refresh_btn.Size = new System.Drawing.Size(37, 36);
+            this.Refresh_btn.TabIndex = 11;
+            this.Refresh_btn.Text = " ";
+            this.Refresh_btn.UseVisualStyleBackColor = true;
+            this.Refresh_btn.Click += new System.EventHandler(this.Refresh_btn_Click);
+            // 
+            // Back_btn
+            // 
+            this.Back_btn.FlatAppearance.BorderSize = 0;
+            this.Back_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back_btn.Image = global::VotingSystem.Properties.Resources.icons8_left_50;
+            this.Back_btn.Location = new System.Drawing.Point(353, 44);
+            this.Back_btn.Name = "Back_btn";
+            this.Back_btn.Size = new System.Drawing.Size(46, 36);
+            this.Back_btn.TabIndex = 9;
+            this.Back_btn.Text = " ";
+            this.Back_btn.UseVisualStyleBackColor = true;
+            this.Back_btn.Click += new System.EventHandler(this.Back_btn_Click);
+            // 
+            // FRM_ViewElectionParticipants
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 360);
+            this.Controls.Add(this.FL_Elections);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRM_ViewElectionParticipants";
+            this.Text = "FRM_ViewElectionParticipants";
+            this.Load += new System.EventHandler(this.FRM_ViewElectionParticipants_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Refresh_btn;
+        private System.Windows.Forms.Button Back_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel FL_Elections;
+    }
+}
---
+++ VotingSystem/ADMIN_AddUpdateElection/FRM_ViewElectionParticipants.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VotingSystem.BLL;
+
+namespace VotingSystem.ADMIN_AddUpdateElection
+{
+    public partial class FRM_ViewElectionParticipants : Form
+    {
+        public FRM_ViewElectionParticipants()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void displayfieldElectionMember(string electionid)
+        {
+            label2.Text = electionid;
+        }
+
+        private void Back_btn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void FL_Elections_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FRM_ViewElectionParticipants_Load(object sender, EventArgs e)
+        {
+            string searchTermsPL = label2.Text;
+
+            FL_Elections.Controls.Clear();
+
+
+            ClassBLL objbll = new ClassBLL();
+
+            DataTable dt = objbll.SearchItemsPL(searchTermsPL);
+
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    UC_ElectionMembers ElectionMembers = new UC_ElectionMembers();
+
+                    //MemoryStream ms = new MemoryStream((byte[])row["CandidatePic"]);
+                    //partylistMembers.MemberPicture = new Bitmap(ms);
+
+                    ElectionMembers.PLID = row["Partylist_ID"].ToString();
+                    //ElectionMembers.PartyName = row["Name"].ToString();
+                   
+
+                    FL_Elections.Controls.Add(ElectionMembers);
+                    
+                    ElectionMembers.Click += new System.EventHandler(this.Refresh_btn_Click);
+
+                }
+            }
+            else
+            {
+                // Show a message or handle no data available
+                MessageBox.Show("No data available.");
+            }
+        }
+
+        private void Refresh_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
