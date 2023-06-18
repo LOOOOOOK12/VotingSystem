bddea1c2a026baa6e473dd4e8983f7ddf48889a6
Luke Symon <lukealamil12@gmail.com>
2023-06-18 14:11:46 +0800

    adjusted size

--- VotingSystem/VOTERS_UserControls/UC_VotingBtn.Designer.cs
+++ VotingSystem/VOTERS_UserControls/UC_VotingBtn.Designer.cs
@@ -53,7 +53,7 @@
             // PB_CandidatePic
             // 
             this.PB_CandidatePic.Image = global::VotingSystem.Properties.Resources.icons8_male_user_96;
-            this.PB_CandidatePic.Location = new System.Drawing.Point(48, 90);
+            this.PB_CandidatePic.Location = new System.Drawing.Point(60, 90);
             this.PB_CandidatePic.Name = "PB_CandidatePic";
             this.PB_CandidatePic.Size = new System.Drawing.Size(109, 101);
             this.PB_CandidatePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -67,7 +67,7 @@
             this.BTN_Vote.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BTN_Vote.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTN_Vote.ForeColor = System.Drawing.Color.White;
-            this.BTN_Vote.Location = new System.Drawing.Point(48, 296);
+            this.BTN_Vote.Location = new System.Drawing.Point(60, 296);
             this.BTN_Vote.Name = "BTN_Vote";
             this.BTN_Vote.Size = new System.Drawing.Size(109, 43);
             this.BTN_Vote.TabIndex = 12;
@@ -79,7 +79,7 @@
             // 
             this.LBL_PartylistName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.LBL_PartylistName.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_PartylistName.Location = new System.Drawing.Point(48, 239);
+            this.LBL_PartylistName.Location = new System.Drawing.Point(60, 239);
             this.LBL_PartylistName.Name = "LBL_PartylistName";
             this.LBL_PartylistName.Size = new System.Drawing.Size(109, 21);
             this.LBL_PartylistName.TabIndex = 13;
@@ -90,7 +90,7 @@
             // 
             this.LBL_Name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.LBL_Name.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_Name.Location = new System.Drawing.Point(49, 207);
+            this.LBL_Name.Location = new System.Drawing.Point(61, 207);
             this.LBL_Name.Name = "LBL_Name";
             this.LBL_Name.Size = new System.Drawing.Size(109, 21);
             this.LBL_Name.TabIndex = 14;
@@ -101,7 +101,7 @@
             // 
             this.LBL_Position.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.LBL_Position.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_Position.Location = new System.Drawing.Point(48, 272);
+            this.LBL_Position.Location = new System.Drawing.Point(60, 272);
             this.LBL_Position.Name = "LBL_Position";
             this.LBL_Position.Size = new System.Drawing.Size(109, 21);
             this.LBL_Position.TabIndex = 15;
@@ -112,7 +112,7 @@
             // 
             this.LBL_EletionTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.LBL_EletionTitle.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_EletionTitle.Location = new System.Drawing.Point(49, 3);
+            this.LBL_EletionTitle.Location = new System.Drawing.Point(61, 3);
             this.LBL_EletionTitle.Name = "LBL_EletionTitle";
             this.LBL_EletionTitle.Size = new System.Drawing.Size(109, 71);
             this.LBL_EletionTitle.TabIndex = 16;
@@ -123,7 +123,7 @@
             // 
             this.Timestamp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Timestamp.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Timestamp.Location = new System.Drawing.Point(48, 347);
+            this.Timestamp.Location = new System.Drawing.Point(60, 347);
             this.Timestamp.Name = "Timestamp";
             this.Timestamp.Size = new System.Drawing.Size(109, 21);
             this.Timestamp.TabIndex = 17;
@@ -145,7 +145,7 @@
             this.Controls.Add(this.PB_PartylistLogo);
             this.Controls.Add(this.PB_CandidatePic);
             this.Name = "UC_VotingBtn";
-            this.Size = new System.Drawing.Size(192, 377);
+            this.Size = new System.Drawing.Size(210, 381);
             ((System.ComponentModel.ISupportInitialize)(this.PB_PartylistLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PB_CandidatePic)).EndInit();
             this.ResumeLayout(false);
