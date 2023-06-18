60f355e30b5447ef9a7993d192c03b632604e7e7
Luke Symon <lukealamil12@gmail.com>
2023-06-18 14:11:12 +0800

    stores the title of the election

---
+++ VotingSystem/VOTERS_UserControls/UC_ElectionsView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VotingSystem.VOTERS_VotingForms;
+
+namespace VotingSystem.VOTERS_UserControls
+{
+    public partial class UC_ElectionsView : UserControl
+    {
+        public UC_ElectionsView()
+        {
+            InitializeComponent();
+        }
+
+        private FlowLayoutPanel FL_Vote;
+
+        public string ViewElection 
+        {
+            get { return LBL_ElectionTitle.Text; }
+            set { LBL_ElectionTitle.Text = value; }
+        }
+
+        private void BTN_View_Click(object sender, EventArgs e)
+        {
+            FRM_VotingForm votingForm = new FRM_VotingForm();
+            votingForm.ShowDialog();
+        }
+
+        private void LBL_ElectionTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
