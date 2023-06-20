542124c6af79fef492738174a20044693f5be342
Luke Symon <lukealamil12@gmail.com>
2023-06-20 23:11:05 +0800

    update

--- VotingSystem/FRM_Home.cs
+++ VotingSystem/FRM_Home.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VotingSystem.BLL;
 
 namespace VotingSystem
 {
@@ -92,33 +94,79 @@ namespace VotingSystem
 
         private void FRM_Home_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True");
-
             timer1.Start();
+
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-SM9NF9V;Initial Catalog=DB_VotingSystem;Integrated Security=True");
 
             con.Open();
             SqlCommand cmd = con.CreateCommand();
+            SqlCommand cmdd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
 
             string query = "SELECT COUNT(votersID) FROM Voters";
 
+            string votedQuery = "SELECT COUNT (Vote_ID) FROM Votes";
+
             try
-            { 
+            {
                 cmd = new SqlCommand(query, con);
+                cmdd = new SqlCommand(votedQuery, con);
                 Int32 rowCount = Convert.ToInt32(cmd.ExecuteScalar());
+                Int32 votes = Convert.ToInt32(cmdd.ExecuteScalar());
+                cmdd.Dispose();
                 cmd.Dispose();
                 con.Close();
 
                 label4.Text = rowCount.ToString();
+                label5.Text = votes.ToString();
 
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
 
+            
+
 
         }
+        private void GenerateDynamicUserControl()
+        {
+            string partylistname = "look";
+
+            FL_Count.Controls.Clear();
+
+            ClassBLL_Votes objBLLC = new ClassBLL_Votes();
+
+            DataTable dt = objBLLC.SearchCount(partylistname);
+
+
+            if (dt != null)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    UC_CountVotes[] listitems = new UC_CountVotes[dt.Rows.Count];
+
+                    for (int i = 0; i < 1; i++)
+                    {
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            listitems[i] = new UC_CountVotes();
+
+                            listitems[i].CountVotes = row["PartylistName"].ToString();
+                            
+
+                            FL_Count.Controls.Add(listitems[i]);
+
+                            listitems[i].Click += new System.EventHandler(this.FRM_Home_Load);
+
+                        }
+                    }
+                }
+            }
+        }
+
+
 
         private void timer1_Tick(object sender, EventArgs e)
         {
